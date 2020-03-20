5b18bcd7a0f09404a5d604db6b247c70113ab586
windynuaa <35166996+windynuaa@users.noreply.github.com>
2020-03-20 11:04:44 +0800

    fix

--- main.c
+++ main.c
@@ -12,10 +12,6 @@
 #include <stdio.h>
 #include "ov2460.h"
 
-
-//uint8_t cam_buf[320 * 240 * 2] __attribute__((aligned(64)));
-//uint8_t lcd_buf[320 * 240 * 2] __attribute__((aligned(64)));
-
 void pll_init()
 {
 	sysctl_pll_set_freq(SYSCTL_PLL0, 800000000UL);
@@ -36,41 +32,27 @@ void system_init()
 	sysctl_enable_irq();
 	uarths_init();
 	gpio_init();
-	//io_mux_init();
 	sysctl_set_spi0_dvp_data(1);
 	dmac_init();
-	dvpInit(12000000);
+	dvpInit(12000000);//for ov2640 addr detecting 用于扫描0V2640硬件地址
 }
 
 
 int main()
 {
 	int a;
-	uint16_t* framebudd;
+	uint16_t* framebuffer;
 	system_init();
-	lcd_init(0, 3, 6, 7, 15000000, 37,  38, 3);
+	lcd_init(0, 3, 6, 7, 15000000, 37,  38, 3);//320w 240h
 	lcd_clear(0xFFFF);
-	a = cambus_scan();
+	a = cambus_scan();//GET OV2640 ADDR 得到2640硬件地址
 	Sipeed_OV2640_begin(a, 24000000);
-	//Sipeed_OV2640_run(1);
-	framebudd = get_k210_dataBuffer();
+	Sipeed_OV2640_run(1);
+	framebuffer = get_k210_dataBuffer();//get display_data_buffer address 得到显示数据地址
 
 	while (1)
 	{
-		//lcd_fill_rectangle(0, 0, 320, 240, RED);
-		//msleep(2000);
-		dvp_start_convert();
 		Sipeed_OV2640_sensor_snapshot();
-		//dvp_finish_convert();
-		//lcd_fill_rectangle(0, 0, 160, 120, RED);
-		//msleep(1000);
-		//dvp_finish_convert();
-		//Sipeed_OV2640_run(0);
-		lcd_draw_picture(0, 0, 320, 240, (uint16_t*)framebudd);
-		//Sipeed_OV2640_run(1);
-		//dvp_start_convert();
-		//msleep(30);
+		lcd_draw_picture(0, 0, 320, 240, (uint16_t*)framebuffer);
 	}
-	
-	//	lcd_draw_picture(0, 0, 320, 240, k210_dataBuffer);
 }
